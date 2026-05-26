370847b909ef558df04dd99b27da5b0f7aa36982
Your Name <you@example.com>
2026-05-27 01:23:08 +0800

    fix: move files to repo root

--- IntrprogBackend-main/_helpers/db.ts
+++ IntrprogBackend-main/_helpers/db.ts
@@ -11,14 +11,8 @@ if (!process.env.DATABASE_URL) {
 }
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+  dialect: 'mysql',
+dialectOptions: {}
 });
 
 const db: any = {};
