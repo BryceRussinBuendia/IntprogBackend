57e60f8e5433ffe6e3c8ee9b6495ca4b4a8ba5cb
Your Name <you@example.com>
2026-05-27 02:43:49 +0800

    fix: update CORS origin URL

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:4200',
+    origin: 'https://frontend-cdmy.onrender.com',
     credentials: true
   })
 );
